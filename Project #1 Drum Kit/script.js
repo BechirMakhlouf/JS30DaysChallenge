17b024bae2471f3d30e0717f8e2de72065ebced9
Bechir Makhlouf <bechirM@proton.me>
2022-11-03 16:05:06 +0100

    added sounds to click events

--- Project #1 Drum Kit/script.js
+++ Project #1 Drum Kit/script.js
@@ -2,9 +2,18 @@
 
 
 const keyPressedHandlingEvent = function (event) {
+    if (event.repeat) {
+        return;
+    }
     const keyPressed = document.querySelector(`div[data-key|="${event.keyCode}"`);
+    const audio = document.querySelector(`audio[data-key|="${event.keyCode}"`);
     console.log(`key pressed: ${event.keyCode}`);
     keyPressed.classList.toggle("playing");
+    if (event.type == "keydown") {
+        audio.play();
+    } else {
+        audio.pause();
+    }
 }
 
 document.addEventListener("keydown", keyPressedHandlingEvent);
